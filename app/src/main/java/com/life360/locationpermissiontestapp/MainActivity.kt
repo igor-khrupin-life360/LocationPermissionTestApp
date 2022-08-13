dd3c39d81064acd1929737398fefa4cb42310e0f
Igor Khrupin <igor@life360.com>
2022-08-12 17:16:32 -0700

    added dialogs

--- app/src/main/java/com/life360/locationpermissiontestapp/MainActivity.kt
+++ app/src/main/java/com/life360/locationpermissiontestapp/MainActivity.kt
@@ -13,6 +13,8 @@ import com.life360.locationpermissiontestapp.databinding.ActivityMainBinding
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 
 class MainActivity : AppCompatActivity() {
@@ -118,10 +120,66 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestBackgroundLocation() {
-        requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+        if (isBackgroundLocationGranted()) {
+            Toast.makeText(applicationContext,"Background Location Granted", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+            val builder =
+                AlertDialog.Builder(this)
+            builder.setTitle("This app needs background location access")
+            builder.setMessage("Please grant location access so this app can detect beacons in the background.")
+            builder.setPositiveButton(android.R.string.ok, null)
+            builder.setOnDismissListener {
+                requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+            }
+            builder.show()
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val builder =
+                    AlertDialog.Builder(this)
+                builder.setTitle("Functionality limited")
+                builder.setMessage("Since background location access has not been granted. \n\nPlease go to Settings -> Applications -> Permissions and grant background location access to this app.")
+                builder.setPositiveButton(android.R.string.ok, null)
+                builder.setOnDismissListener {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri: Uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    // This will take the user to a page where they have to click twice to drill down to grant the permission
+                    startActivity(intent)
+                }
+                builder.show()
+            }
+        }
     }
 
     private fun requestForegroundLocation() {
-        requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
+        if (isLocationPermissionGranted()) {
+            Toast.makeText(applicationContext,"Foreground Location Granted", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            val builder =
+                AlertDialog.Builder(this)
+            builder.setTitle("This app needs foreground location access")
+            builder.setMessage("Please grant location access so this app")
+            builder.setPositiveButton(android.R.string.ok, null)
+            builder.setOnDismissListener {
+                requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
+            }
+            builder.show()
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val builder =
+                    AlertDialog.Builder(this)
+                builder.setTitle("Functionality limited")
+                builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons in the background. \n\nPlease go to Settings -> Applications -> Permissions and grant background location access to this app.")
+                builder.setPositiveButton(android.R.string.ok, null)
+                builder.setOnDismissListener {
+                    openLocationPermissionSettings()
+                }
+                builder.show()
+            }
+        }
     }
 }
